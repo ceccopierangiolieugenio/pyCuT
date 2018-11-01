6fab918c07f8843152d33dcec299cf700b1f2a98
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2018-11-01 15:31:06 +0000

    Refine CuEvent

--- cupy/CuTCore/CuEvent.py
+++ cupy/CuTCore/CuEvent.py
@@ -189,48 +189,48 @@ class CuEvent:
 	ref: http://doc.qt.io/qt-5/qmouseevent.html#x
 '''
 class CuMouseEvent(CuEvent):
-	__slots__ = ('_localPos', '_windowPos', '_screenPos', '_globalPos', '_button')
-	def __init__(self, type=None, localPos=None, windowPos=None, screenPos=None, button=CuT.NoButton):
-		CuEvent.__init__(self, type=type )
-		self._localPos  = localPos  if localPos  is not None else CuPoint(0, 0)
-		self._windowPos = windowPos if windowPos is not None else CuPoint(0, 0)
-		self._screenPos = screenPos if screenPos is not None else CuPoint(0, 0)
-		self._globalPos = screenPos if screenPos is not None else CuPoint(0, 0)
-		self._button=button
+	__slots__ = ('l', 'w', 's', 'b')
+
+	def __init__(self, *args, **kwargs):
+		CuEvent.__init__(self, *args, **kwargs)
+		self.l = kwargs.get('localPos', CuPoint(0, 0))
+		self.w = kwargs.get('windowPos', CuPoint(0, 0))
+		self.s = kwargs.get('screenPos', CuPoint(0, 0))
+		self.b = kwargs.get('button', CuT.NoButton)
 
 	def globalPos(self):
-		return self._globalPos
+		return self.s
 
 	# Returns the global x position of the mouse cursor at the time of the event.
 	def globalX(self):
-		return self._globalPos.x()
+		return self.s.x()
 
 	# Returns the global y position of the mouse cursor at the time of the event.
 	def globalY(self):
-		return self._globalPos.y()
+		return self.s.y()
 
 	# Returns the position of the mouse cursor, relative to the widget that received the event.
 	def pos(self):
-		return self._localPos
+		return self.l
 
 	# Returns the position of the mouse cursor as a Point, relative to the screen that received the event.
 	def screenPos(self):
-		return self._screenPos
+		return self.s
 
 	# Returns the position of the mouse cursor as a Point, relative to the window that received the event.
 	def windowPos(self):
-		return self._windowPos
+		return self.w
 
 	# Returns the x position of the mouse cursor, relative to the widget that received the event.
 	def x(self):
-		return self._localPos.x()
+		return self.l.x()
 
 	# Returns the y position of the mouse cursor, relative to the widget that received the event.
 	def y(self):
-		return self._localPos.y()
+		return self.l.y()
 
 	def button(self):
-		return self._button
+		return self.b
 
 
 ''' CuInputEvent
